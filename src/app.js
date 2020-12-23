ecba0f95a94095d3bb686732a78eca03d6119a23
Jonathan Herrera <jonysteel99@gmail.com>
2020-12-23 13:57:11 -0600

    Tercera Subida

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const { urlencoded } = require('express');
 
 // settings
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 443);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
